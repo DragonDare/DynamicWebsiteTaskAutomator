b9e0d2aedf48dfb5dde100418d0bd08a0a4b89ae
DragonDare <deykohav@gmail.com>
2021-12-25 18:06:32 +0530

    Imported libraries and made dummy html file

--- male_automate.py
+++ male_automate.py
@@ -1,11 +1,3 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-url = "https://github.com/html5lib/"
-
-page = "try1.html"
-
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-
-print(html)
