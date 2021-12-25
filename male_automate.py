23e43edd0ecfdc4b611492e87c2c3d7bbf26db56
DragonDare <deykohav@gmail.com>
2021-12-25 18:21:52 +0530

    Random website HTML Scrape

--- male_automate.py
+++ male_automate.py
@@ -1,3 +1,12 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from bs4 import BeautifulSoup   #Web Scraper
+from urllib.request import urlopen  #URL fetch
+import re   #Regular Expressions
 
+url = "https://github.com/html5lib/"
+
+page = urlopen(url)
+
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+
+print(html)
